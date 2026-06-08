3a1d6c09ecdb3574b67de6820569850455a7506d
ItsFelix5 <60808107+ItsFelix5@users.noreply.github.com>
2026-06-08 19:15:52 +0200

    Pronoun plugin (fr)

--- plugins/Pronouns.tsx
+++ plugins/Pronouns.tsx
@@ -12,49 +12,30 @@ export default class UserPronouns extends TautPlugin {
     }
   `
   static readonly authors = '<@U07AGEVSTD2>'
-
-  private cache = this.api.createCache<string | undefined>('user_pronouns', {
-    ttl: 24 * 60 * 60 * 1000,
-    maxSize: 5000,
-  })
-
   private unpatchBaseMessageSender = () => {}
 
   start() {
     this.log('Started')
-
-    this.cache.load()
+    const getState = (
+      Object.values(
+        this.api.findExport(
+          (exp) =>
+            typeof exp == 'function' && exp.name === 'getStoreInstanceMap'
+        )()
+      )[0] as any
+    )?.getState
 
     this.unpatchBaseMessageSender = this.api.patchComponent<{
-      botId?: string
-      userId?: string
-      className?: string
+      visible: boolean
+      userId: string
     }>('BaseMessageSender', (OriginalBaseMessageSender) => (props) => {
-      const userId = props.userId
-      const [pronouns, setPronouns] = React.useState<string | undefined>(() => {
-        if (!userId) return undefined
-        return this.cache.get(userId)
-      })
-
-      React.useEffect(() => {
-        if (!userId) return
-        const state = (
-          Object.values(
-            this.api.findExport(
-              (exp) =>
-                typeof exp == 'function' && exp.name === 'getStoreInstanceMap'
-            )()
-          )[0] as any
-        )?.getState()
-        const pronouns = state?.members?.[userId]?.profile?.pronouns?.trim()
-        if (pronouns) setPronouns(pronouns)
-        else console.log('No pronouns found for', userId)
-      }, [userId])
+      const pronouns =
+        getState()?.members?.[props.userId]?.profile?.pronouns?.trim()
 
       return (
         <>
-          <OriginalBaseMessageSender {...props} className={props.className} />
-          {pronouns && (
+          <OriginalBaseMessageSender {...props} />
+          {props.visible && pronouns && (
             <>
               {'  '}
               <span className="c-timestamp c-timestamp__label">
